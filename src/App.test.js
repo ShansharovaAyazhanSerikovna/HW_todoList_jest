d86e6af8c796a4b631a6b98a3337488c8cd5509a
Ayazhan <shansharova_ayazhan@mail.ru>
2025-03-27 11:22:53 +0500

    test

--- src/App.test.js
+++ src/App.test.js
@@ -4,10 +4,38 @@ import App from './App';
 test('Жаңа тапсырма қосу', () => {
   render(<App />);
   const input = screen.getByPlaceholderText("Жаңа тапсырма...");
-  const button = screen.getByText("Қосу")
+  const button = screen.getByText("Қосу");
 
-  fireEvent.change(input, {target: {value: " Реакт үйрену"}})
-  fireEvent.click(button)
+  fireEvent.change(input, {target: {value: " Реакт үйрену"}});
+  fireEvent.click(button);
 
   expect(screen.getByText("Реакт үйрену")).toBeInTheDocument();
 });
+
+test('Тапсырманы белгілеу', () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText("Жаңа тапсырма...");
+  const button = screen.getByText("Қосу")
+
+  fireEvent.change(input, {target: {value: " Реакт үйрену"}});
+  fireEvent.click(button);
+
+  const checkbox = screen.getByRole("checkbox");
+  fireEvent.click(checkbox)
+
+  expect(checkbox).toBeChecked();
+});
+
+
+test("Тапсырманы өшіру", ()=>{
+  const input = screen.getByPlaceholderText("Жаңа тапсырма...");
+  const button = screen.getByText("Қосу");
+
+  fireEvent.change(input, {target:{value: "JEST тест жазу"}});
+  fireEvent.click(button);
+
+  const deleteButton = screen.getByText("Өшіру");
+  fireEvent.click(deleteButton);
+
+  expect(screen.queryByText("JEST тест жазу")).not.toBeInTheDocument();
+})
